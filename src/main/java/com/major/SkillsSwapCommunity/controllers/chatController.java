7912ed681c5bd8660bc10a0543db6f706b8c9db4
bhaskar katara <bhaskarkatararjo5@gmail.com>
2025-05-24 11:36:01 +0530

    add chat controller

---
+++ src/main/java/com/major/SkillsSwapCommunity/controllers/chatController.java
@@ -0,0 +1,13 @@
+package com.major.SkillsSwapCommunity.controllers;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/chat")
+public class chatController {
+
+}
