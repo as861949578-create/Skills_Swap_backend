136a981bbab19640c4c4d1d5d6dbe0a948626e5e
bhaskar katara <bhaskarkatararjo5@gmail.com>
2025-05-24 14:54:58 +0530

    add

--- src/main/java/com/major/SkillsSwapCommunity/WebSocketConfig.java
+++ src/main/java/com/major/SkillsSwapCommunity/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws-chat").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
