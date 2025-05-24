c0dd1fbba250fc48d24912d80cd1e70f3693c84f
bhaskar katara <bhaskarkatararjo5@gmail.com>
2025-05-24 14:48:52 +0530

    add webSocket

--- src/main/java/com/major/SkillsSwapCommunity/ChatWebSocketController.java
+++ src/main/java/com/major/SkillsSwapCommunity/ChatWebSocketController.java
@@ -6,8 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin(origins = "*")
+@Controller
 public class ChatWebSocketController {
 
     @Autowired
